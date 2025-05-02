7819923f5b5b90d6615474c0d0ddca2293af80fa
taeyun <lwe12f675@gmail.com>
2025-05-03 02:18:09 +0900

    feat: 로그인 모달 스타일 적용 및 클리이언트 컴포넌트로 전환

--- src/app/(beforLogin)/layout.tsx
+++ src/app/(beforLogin)/layout.tsx
@@ -8,7 +8,6 @@ type Props = {
 export default function Layout({ children, modal }: Props) {
   return (
     <div className={styles.container}>
-      비포 로그인 레이아웃
       {children}
       {modal}
     </div>
