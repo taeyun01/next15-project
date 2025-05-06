b5a1dc202af053397e3569923c264c64324d3461
taeyun <lwe12f675@gmail.com>
2025-05-07 02:39:59 +0900

    feat: Post구현 및 dayjs적용

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -1,3 +1,56 @@
+import style from "./post.module.css";
+import Link from "next/link";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import "dayjs/locale/ko";
+// import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
+import Image from "next/image";
+
+dayjs.locale("ko");
+dayjs.extend(relativeTime);
+
 export default function Post() {
-  return <div>포스트</div>;
+  const target = {
+    User: {
+      id: "elonmusk",
+      nickname: "Elon Musk",
+      image: "/logo.jpg",
+    },
+    content: "안녕하세요 일론 머스크 입니다! 반갑습니다!",
+    createdAt: new Date(),
+    Images: [],
+  };
+  return (
+    <article className={style.post}>
+      <div className={style.postWrapper}>
+        <div className={style.postUserSection}>
+          <Link href={`/${target.User.id}`} className={style.postUserImage}>
+            <Image
+              src={target.User.image}
+              alt={target.User.nickname}
+              width={40}
+              height={40}
+            />
+            <div className={style.postShade} />
+          </Link>
+        </div>
+        <div className={style.postBody}>
+          <div className={style.postMeta}>
+            <Link href={`/${target.User.id}`}>
+              <span className={style.postUserName}>{target.User.nickname}</span>
+              &nbsp;
+              <span className={style.postUserId}>@{target.User.id}</span>
+              &nbsp; · &nbsp;
+            </Link>
+            <span className={style.postDate}>
+              {dayjs(target.createdAt).fromNow(true)}
+            </span>
+          </div>
+          <div>{target.content}</div>
+          <div className={style.postImageSection}></div>
+          {/* <ActionButtons /> */}
+        </div>
+      </div>
+    </article>
+  );
 }
--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -10,7 +10,7 @@ const HomePage = () => {
       <TabProvider>
         <Tap />
         <PostForm />
-        {/* <Post /> */}
+        <Post />
       </TabProvider>
     </main>
   );
