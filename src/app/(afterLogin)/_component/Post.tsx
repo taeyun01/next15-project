30b441f61d21064f1ebf096a123bf0564615d8eb
taeyun <lwe12f675@gmail.com>
2025-05-06 23:57:34 +0900

    feat: 홈 탭 적용 후 Context API 적용

---
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -0,0 +1,3 @@
+export default function Post() {
+  return <div>포스트</div>;
+}
--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,5 +1,22 @@
+import style from "./home.module.css";
+import Tap from "./_component/Tab";
+import PostForm from "./_component/PostForm";
+import Post from "../_component/Post";
+import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
+
 const HomePage = () => {
-  return <div>홈페이지</div>;
+  return (
+    <main className={style.main}>
+      <TabProvider>
+        <Tap />
+        {/* <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post /> */}
+      </TabProvider>
+    </main>
+  );
 };
 
 export default HomePage;
