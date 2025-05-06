dc0245707aba177d11c2e0da867d976a76195dca
taeyun <lwe12f675@gmail.com>
2025-05-07 04:41:51 +0900

    feat: 내 프로필 그리고 /search페이지 구현 (useSearchParams()훅은 15버전에서 비동기로 바뀜)

--- src/app/(afterLogin)/[username]/page.tsx
+++ src/app/(afterLogin)/[username]/page.tsx
@@ -1,3 +1,39 @@
-export default function UserProfile() {
-  return <div>UserProfile</div>;
+import style from "./profile.module.css";
+import Post from "@/app/(afterLogin)/_component/Post";
+import BackButton from "@/app/(afterLogin)/_component/BackButton";
+import Image from "next/image";
+
+export default function Profile() {
+  const user = {
+    id: "taeyun",
+    nickname: "유태윤",
+    image: "/tlogo.png",
+  };
+
+  return (
+    <main className={style.main}>
+      <div className={style.header}>
+        <BackButton />
+        <h3 className={style.headerTitle}>{user.nickname}</h3>
+      </div>
+      <div className={style.userZone}>
+        <div className={style.userImage}>
+          <Image src={user.image} alt={user.id} width={134} height={134} />
+        </div>
+        <div className={style.userName}>
+          <div>{user.nickname}</div>
+          <div>@{user.id}</div>
+        </div>
+        <button className={style.followButton}>팔로우</button>
+      </div>
+      <div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </main>
+  );
 }
--- src/app/(afterLogin)/search/page.tsx
+++ src/app/(afterLogin)/search/page.tsx
@@ -1,3 +1,37 @@
-export default function Search() {
-  return <div>Search</div>;
+import style from "./search.module.css";
+import BackButton from "@/app/(afterLogin)/_component/BackButton";
+import SearchForm from "@/app/(afterLogin)/_component/SearchForm";
+import Tab from "@/app/(afterLogin)/search/_component/Tab";
+import Post from "@/app/(afterLogin)/_component/Post";
+
+type Props = {
+  searchParams: Promise<{ q: string; f?: string; pf?: string }>;
+};
+export default async function Search({ searchParams }: Props) {
+  const { q } = await searchParams; // next15부터 비동기로 바뀜
+
+  console.log("searchParams: ", q);
+  return (
+    <main className={style.main}>
+      <div className={style.searchTop}>
+        <div className={style.searchZone}>
+          <div className={style.buttonZone}>
+            <BackButton />
+          </div>
+          <div className={style.formZone}>
+            <SearchForm q={q} />
+          </div>
+        </div>
+        <Tab />
+      </div>
+      <div className={style.list}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        {/*<SearchResult searchParams={searchParams} />*/}
+      </div>
+    </main>
+  );
 }
