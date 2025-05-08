597469efca2ff074b103ce7c762d74ece7f19184
taeyun <lwe12f675@gmail.com>
2025-05-09 04:17:52 +0900

    feat: 서버 컴포넌트에서 Server Actions 사용

--- src/app/(beforeLogin)/_component/LoginModal.tsx
+++ src/app/(beforeLogin)/_component/LoginModal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import BackButton from "@/app/(beforeLogin)/_component/BackButton";
 import style from "./login.module.css";
 import { useState } from "react";
 
@@ -8,7 +9,6 @@ export default function LoginModal() {
   const [password, setPassword] = useState();
   const [message, setMessage] = useState();
   const onSubmit = () => {};
-  const onClickClose = () => {};
 
   const onChangeId = () => {};
 
@@ -18,18 +18,7 @@ export default function LoginModal() {
     <div className={style.modalBackground}>
       <div className={style.modal}>
         <div className={style.modalHeader}>
-          <button className={style.closeButton} onClick={onClickClose}>
-            <svg
-              width={24}
-              viewBox="0 0 24 24"
-              aria-hidden="true"
-              className="r-18jsvk2 r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03"
-            >
-              <g>
-                <path d="M10.59 12L4.54 5.96l1.42-1.42L12 10.59l6.04-6.05 1.42 1.42L13.41 12l6.05 6.04-1.42 1.42L12 13.41l-6.04 6.05-1.42-1.42L10.59 12z"></path>
-              </g>
-            </svg>
-          </button>
+          <BackButton />
           <div>로그인하세요.</div>
         </div>
         <form onSubmit={onSubmit}>
