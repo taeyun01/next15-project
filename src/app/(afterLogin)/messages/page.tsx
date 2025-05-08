2a341c34c7b89d8711f80c06d0de1cfd869cbe2e
taeyun <lwe12f675@gmail.com>
2025-05-08 14:43:37 +0900

    feat: /messages 쪽지 페이지 마크업

--- src/app/(afterLogin)/messages/page.tsx
+++ src/app/(afterLogin)/messages/page.tsx
@@ -1,3 +1,16 @@
+import style from "./message.module.css";
+import Room from "@/app/(afterLogin)/messages/_component/Room";
+
 export default function Messages() {
-  return <div>Messages</div>;
+  return (
+    <main className={style.main}>
+      <div className={style.header}>
+        <h3>쪽지</h3>
+      </div>
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+    </main>
+  );
 }
