50f2f578e39cec26ac8df63223e5f3e676242cdd
taeyun <lwe12f675@gmail.com>
2025-05-11 02:31:50 +0900

    feat: react-query를 쓰는 이유와 fresh, stale, inactive란

--- src/app/(afterLogin)/_component/RQProvider.tsx
+++ src/app/(afterLogin)/_component/RQProvider.tsx
@@ -12,12 +12,12 @@ const RQProvider = ({ children }: Props) => {
   const [client] = useState(
     new QueryClient({
       defaultOptions: {
-        // react-query 전역 설정
+        // react-query 전역 설정 (useQuery에서 덮어쓰기 가능 즉, 개별 쿼리에서 설정 가능)
         queries: {
-          refetchOnWindowFocus: false,
-          retryOnMount: true,
-          refetchOnReconnect: false,
-          retry: false,
+          refetchOnWindowFocus: false, // 탭 전환 시 데이터를 새로 불러올건지 여부
+          retryOnMount: true, // 컴포넌트가 마운트 될 때 데이터를 다시 불러올건지 여부 (페이지를 이동했던지, 컴포넌트가 state떄문에 언마운트 됐다가 마운트 됐던지)
+          refetchOnReconnect: false, // 인터넷 연결이 끊어졌다가 다시 연결될 때 데이터를 새로 불러올건지 여부
+          retry: false, // 데이터를 불러오는데 실패했을 때 몇 번 재시도할지 여부 (false일 경우 재시도 안하고 그냥 에러페이지 보여주는걸로 해도됨)
         },
       },
     })
--- src/app/(afterLogin)/home/_component/PostRecommends.tsx
+++ src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -9,7 +9,15 @@ const PostRecommends = () => {
   const { data } = useQuery<IPost[]>({
     queryKey: ["posts", "recommends"],
     queryFn: getPostRecommends,
+    staleTime: 60 * 1000, // 60초 동안 캐시 유지. (1분이 지나고 새로고침을 하면 서버로 부터 데이터를 새로 가져옴) 컨텐츠 특성에 따라 조정하면됨
+    // staleTime: Infinity, // 한번 데이터를 가져오면 캐시된 데이터만 가져옴. 새로고침 되도 (데이터가 변경되지 않는 경우 사용)
+    gcTime: 300 * 1000, // 기본값은 5분으로 설정 되어있음. (서버에서 받은 데이터들이 메모리에 캐시 되어 있을건데 데이터를이 너무 많아지면 메모리가 터져서 웹사이트가 멈출 수 있음.)
+    // Inactive 상태일 때, gcTime이 돌아가기 시작함. 즉, data를 사용하지 않는 화면에 이동을 했을때 gcTime이 돌아가고 5분뒤에 메모리에서 정리가됨 (inactive란 지금 보는 화면에서 queryKey: ["posts", "recommends"] 이 쿼리를 사용하지 않는다면 inactive상태가 됨. 즉 해당 쿼리를 사용하지 않는 다른 화면으로 넘어갔을 때. 그냥 이 쿼리를 쓰고 있냐 안쓰고 있냐 상태임)
+    // 만약 5분 사이에 돌아오면 캐시되어있던 데이터를 바로 보여주지만, 5분이 지나면 메모리에서 정리가 되면 캐시가 날아가기 때문에 데이터를 새로 불러옴
+    //* staleTime은 항상 gcTime보다 짧아야한다.
   });
+
+  console.log(data);
 
   return data?.map((post) => <Post key={post.postId} post={post} />);
 };
